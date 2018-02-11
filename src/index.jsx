0a5105a47351bccf95ede6d972c54fa695382ffe
erinknight <erin.knight@headspring.com>
2018-02-11 15:32:03 -0600

    Add constants and initial state

--- src/index.jsx
+++ src/index.jsx
@@ -13,7 +13,7 @@ const testData = {
   },
   cards: [
     { id: 1, title: 'Foo', msg: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin cursus vitae velit a congue.', imgUrl: 'https://picsum.photos/500/300' },
-    { id: 2, title: 'Bar', msg: 'Etiam pretium a elit in varius. Ut vel est accumsan, interdum erat sit amet, lacinia risus.', imgUrl: 'https://picsum.photos/500/301' },
+    { id: 2, title: 'Bar', msg: 'Etiam pretium a elit in varius. Ut vel est accumsan, interdum erat sit amet, lacinia risus.', imgUrl: 'https://picsum.photos/500/300/?random' },
   ],
 };
 
